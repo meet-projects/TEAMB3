58472bf9fe8db16c073513a5c6dc73c5496a83ab
Kiosk <kiosk@cs.huji.ac.il>
2015-08-09 17:15:33 +0300

    finshed lab 7

--- web_app.py
+++ web_app.py
@@ -15,17 +15,42 @@ session = DBSession()
 def main():
 	return render_template('main_page.html')
 
-@app.route('/profile')
-def view_profile():
-	return render_template('view_profile.html')
+@app.route('/profile/<int:user_id>')
+def view_profile(user_id):
+	person = session.query(User).filter_by(id=user_id).first()
+	return render_template('view_profile.html', person = person)
 
-@app.route('/edit')
-def edit_profile():
-	return render_template('edit_profile.html')
+@app.route('/edit/<int:user_id>', methods=['GET', 'POST'])
+def edit_profile(user_id):
+	person = session.query(User).filter_by(id=user_id).first()
+	if request.method == 'GET':
+		return render_template('edit_profile.html', person = person)
+	else:
+        	# read form data
+        	new_firstname = request.form['firstname']
+        	new_lastname = request.form['lastname']
+        	new_username = request.form['username']
+        	new_password = request.form['password']
+		new_bio = request.form['bio']
 
-@app.route('/login')
+        	# MISSING CODE HERE FOR UPDATING THE USER
+		new_person = session.query(User).filter_by(id = user_id).first()
+		person.first_name = new_firstname
+		person.last_name = new_lastname
+		person.username = new_username
+		person.password = new_password
+		person.bio = new_bio
+
+        	# redirect user to the page that views the user's new profile
+        	return redirect(url_for('view_profile'))
+
+#fix login page after everything is done
+@app.route('/login', methods=['GET', 'POST'])
 def login_page():
-	return render_template('login_page.html')
+	if request.method == 'GET':
+		return render_template('login_page.html')
+	else:
+		
 
 @app.route('/delete')
 def delete_profile():
@@ -33,3 +58,8 @@ def delete_profile():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
